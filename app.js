00e48b25c9b0ab791710296b44350eb7552ec982
deqiq <kibdemta@gmail.com>
2017-10-17 03:58:53 +0300

    bind to an environment variable

--- app.js
+++ app.js
@@ -593,7 +593,7 @@ app.get('/isubject3', function (req, res) {
  })
 
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8081, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
